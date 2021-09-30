601753ebefd0e21ae9609b5d6f25281543548682
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-09-30 11:19:10 +0800

    add log descriptions

--- src/main/kotlin/commands/LogCommand.kt
+++ src/main/kotlin/commands/LogCommand.kt
@@ -1,13 +1,11 @@
 package me.iori.minori.commands
 
 import me.iori.minori.Minori
-import me.iori.minori.commands.LogCommand.build
 import me.iori.minori.records.Recorder
 import net.mamoe.mirai.console.command.CommandSender
 import net.mamoe.mirai.console.command.CompositeCommand
 import net.mamoe.mirai.console.command.getBotOrNull
 import net.mamoe.mirai.console.command.getGroupOrNull
-import net.mamoe.mirai.console.terminal.consoleLogger
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 import net.mamoe.mirai.contact.User
 import net.mamoe.mirai.contact.nameCardOrNick
@@ -26,19 +24,25 @@ object LogCommand : CompositeCommand(
   override val prefixOptional = true
 
   @SubCommand
+  @Description("返回群聊中最近几条消息")
   suspend fun CommandSender.last(k: Int) {
     val group = this.getGroupOrNull() ?: return
 
     val messages = Recorder.recentMessage(group.id, k)
     val forward = buildForwardMessage(group) {
       messages.forEach {
-        add(it.sender, group[it.sender]?.nameCardOrNick ?: "${it.sender}", it.content.deserializeMiraiCode())
+        add(
+          it.sender,
+          group[it.sender]?.nameCardOrNick ?: "${it.sender}",
+          it.content.deserializeMiraiCode()
+        )
       }
     }
     sendMessage(forward)
   }
 
   @SubCommand
+  @Description("返回群聊某用户最近几条消息")
   suspend fun CommandSender.last(user: User, k: Int) {
     val group = this.getGroupOrNull() ?: return
     val messages = Recorder.memberMessage(group.id, user.id, k)
@@ -51,6 +55,7 @@ object LogCommand : CompositeCommand(
   }
 
   @SubCommand
+  @Description("伪造聊天记录（单人）")
   suspend fun CommandSender.build(user: User, vararg messages: Message) {
     val group = this.getGroupOrNull() ?: return
     val forward = buildForwardMessage(group) {
@@ -64,6 +69,7 @@ object LogCommand : CompositeCommand(
   }
 
   @SubCommand
+  @Description("伪造聊天记录（多人）")
   suspend fun CommandSender.builds(vararg messages: SingleMessage) {
     val group = this.getGroupOrNull() ?: return
     val bot = this.getBotOrNull() ?: return
