a0741d97118095d397ec89ee3831a6476598eb44
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-09-30 19:11:00 +0800

    add setu recall

--- src/main/kotlin/commands/SetuCommand.kt
+++ src/main/kotlin/commands/SetuCommand.kt
@@ -14,16 +14,24 @@ import net.mamoe.mirai.message.data.content
 
 import me.iori.minori.Minori
 import me.iori.minori.utils.Network
+import kotlin.random.Random
 
 object SetuCommand : RawCommand(
   owner = Minori,
   description = "获取色图",
-  primaryName = "色图"
+  primaryName = "色图",
+  prefixOptional = true,
+  usage = "(/)色图 <tags?>    # 根据标签获取色图"
 ) {
   private const val URL = "https://api.lolicon.app/setu/v2"
+  private const val QUALITY = "original"
+  private const val STORAGE_PATH = "data/Minori/setus"
 
   @Serializable
   data class Picture(
+    val pid: Int,
+    val uid: Int,
+    val ext: String,
     val urls: Map<String, String>
   )
 
@@ -37,6 +45,7 @@ object SetuCommand : RawCommand(
     val tags = if (args.isEmpty()) listOf() else args.map { it.content }
     val r: Response = Network.get(URL) {
       parameter("r18", 0)
+      parameter("size", QUALITY)
       if (tags.isNotEmpty()) parameter("tag", tags.joinToString("|"))
     }
 
@@ -44,22 +53,27 @@ object SetuCommand : RawCommand(
       sendMessage("没有找到相关色图")
       return
     }
-    val urlString = r.data[0].urls["original"]
-    if (urlString == null) {
-      sendMessage("没有找到相关色图")
+
+    if (r.error.isNotEmpty()) {
+      sendMessage("色图获取失败：${r.error}")
       return
     }
+
+    val img = r.data.first()
+    val urlString = img.urls[QUALITY] as String
     try {
       val rawImage = Network.get<ByteArray>(urlString)
-      val file = File("image.jpg")
+      val file = File("$STORAGE_PATH/${img.pid}${img.uid}.${img.ext}")
 
       withContext(Dispatchers.IO) {
         @Suppress("BlockingMethodInNonBlockingContext") // 麻了
         file.outputStream().write(rawImage)
       }
       subject?.sendImage(rawImage.inputStream())
+        ?.recallIn((Random.nextInt(20, 40) * 1000).toLong())
     } catch (_: ConnectTimeoutException) {
       sendMessage("网路发生了异常，色图的链接为$urlString")
+        ?.recallIn((Random.nextInt(20, 40) * 1000).toLong())
     }
   }
 }
