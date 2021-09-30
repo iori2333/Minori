b2bdcc2b31607e47134e3f74e5281cb9e5b26ae4
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-09-30 17:32:09 +0800

    add SetuCommand

--- src/main/kotlin/commands/SetuCommand.kt
+++ src/main/kotlin/commands/SetuCommand.kt
@@ -1,23 +1,65 @@
 package me.iori.minori.commands
 
-import io.ktor.client.*
-import me.iori.minori.Minori
+import java.io.File
+import io.ktor.client.request.*
+import io.ktor.network.sockets.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.Serializable
 import net.mamoe.mirai.console.command.CommandSender
 import net.mamoe.mirai.console.command.RawCommand
+import net.mamoe.mirai.contact.Contact.Companion.sendImage
 import net.mamoe.mirai.message.data.MessageChain
 import net.mamoe.mirai.message.data.content
+
+import me.iori.minori.Minori
+import me.iori.minori.utils.Network
 
 object SetuCommand : RawCommand(
   owner = Minori,
   description = "获取色图",
   primaryName = "色图"
 ) {
+  private const val URL = "https://api.lolicon.app/setu/v2"
+
+  @Serializable
+  data class Picture(
+    val urls: Map<String, String>
+  )
+
+  @Serializable
+  data class Response(
+    val error: String,
+    val data: List<Picture>,
+  )
+
   override suspend fun CommandSender.onCommand(args: MessageChain) {
-    if (args.isEmpty()) {
-      sendMessage("empty setu")
+    val tags = if (args.isEmpty()) listOf() else args.map { it.content }
+    val r: Response = Network.get(URL) {
+      parameter("r18", 0)
+      if (tags.isNotEmpty()) parameter("tag", tags.joinToString("|"))
+    }
+
+    if (r.data.isEmpty()) {
+      sendMessage("没有找到相关色图")
       return
     }
-    val tags = args.map { it.content }
-    println(tags.joinToString())
+    val urlString = r.data[0].urls["original"]
+    if (urlString == null) {
+      sendMessage("没有找到相关色图")
+      return
+    }
+    try {
+      val rawImage = Network.get<ByteArray>(urlString)
+      val file = File("image.jpg")
+
+      withContext(Dispatchers.IO) {
+        @Suppress("BlockingMethodInNonBlockingContext") // 麻了
+        file.outputStream().write(rawImage)
+      }
+      subject?.sendImage(rawImage.inputStream())
+    } catch (_: ConnectTimeoutException) {
+      sendMessage("网路发生了异常，色图的链接为$urlString")
+    }
   }
 }
--- src/main/kotlin/utils/Network.kt
+++ src/main/kotlin/utils/Network.kt
@@ -1,24 +1,31 @@
 package me.iori.minori.utils
 
 import io.ktor.client.*
-import io.ktor.client.features.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
-import io.ktor.http.*
 
 object Network {
   lateinit var client: HttpClient
 
   fun load() {
-    client = HttpClient()
+    client = HttpClient(CIO) {
+      install(JsonFeature) {
+        serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
+          prettyPrint = true
+          isLenient = true
+          ignoreUnknownKeys = true
+        })
+      }
+    }
   }
 
   fun dispose() {
     client.close()
   }
 
-  suspend inline fun <reified T> get(urlString: String): T {
+  suspend inline fun <reified T> get(urlString: String, block: HttpRequestBuilder.() -> Unit = {}): T {
     return client.get(urlString)
   }
-
-
 }
