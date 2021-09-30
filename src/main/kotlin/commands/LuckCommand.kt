3ba79a77df3ed4eb489383d4b7cfdd7aa76c4d22
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-09-30 11:18:52 +0800

    update LuckCommand

--- src/main/kotlin/commands/LuckCommand.kt
+++ src/main/kotlin/commands/LuckCommand.kt
@@ -1,9 +1,13 @@
 package me.iori.minori.commands
 
 import me.iori.minori.Minori
+import me.iori.minori.data.LanguageData
 import net.mamoe.mirai.console.command.CommandSender
 import net.mamoe.mirai.console.command.SimpleCommand
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
+import net.mamoe.mirai.contact.nameCardOrNick
+import java.util.*
+import kotlin.random.Random
 
 object LuckCommand : SimpleCommand(
   owner = Minori,
@@ -18,6 +22,25 @@ object LuckCommand : SimpleCommand(
 
   @Handler
   suspend fun CommandSender.onCommand() {
-    sendMessage("今日运势")
+    val calendar = Calendar.getInstance()
+    calendar.set(
+      calendar.get(Calendar.YEAR),
+      calendar.get(Calendar.MONTH),
+      calendar.get(Calendar.DATE),
+      0, 0, 0
+    )
+    val seed = calendar.timeInMillis / 1000 + (user?.id ?: 0)
+    val random = Random(seed)
+    val rank = LanguageData.ranks.random(random)
+    val shuffled = LanguageData.events.shuffled(random).subList(0, 6)
+
+    val send = """
+      ${user?.nameCardOrNick ?: user?.id ?: 0}的今日运势：
+      $rank
+      宜：${shuffled.subList(0, 3).joinToString("、")}
+      忌：${shuffled.subList(3, 6).joinToString("、")}
+    """.trimIndent()
+
+    sendMessage(send)
   }
 }
